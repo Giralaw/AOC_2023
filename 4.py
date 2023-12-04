014dbd463ed77fa1d44446817784660102f6b058
Giratinalawyer <al.skeldon@gmail.com>
2023-12-04 00:49:26 -0500

    Day 4 solve

--- 4.py
+++ 4.py
@@ -16,7 +16,53 @@ p1 = 0
 p2 = 0
 
 for line in lines:
-    pass
+    w,l = line.split('|')
+    w = w.split()[2:]
+    l = l.split()
+    # print(w)
+    # print(l)
+    val = -1
+    for n in l:
+        if n in w:
+            val +=1
+    if val > -1:
+        p1 += 2**val
+
+
+cards = list()
+
+
+for i in range(len(lines)):
+    cards.append(i)
+
+p2 = 0
+
+while len(cards) > 0:
+    c = cards.pop()
+    # print(c)
+    p2 += 1
+    val = 0
+    w,l = lines[c].split('|')
+    w = w.split()[2:]
+    l = l.split()
+    for n in l:
+        if n in w:
+            val +=1
+    # print("card",c,val)
+    if val > 0:
+        for j in range(1,val+1):
+            # print((i,j))
+            cards.append(c+j)
+            # print(cards)
+    # cards.sort()
+    # print(cards)
 
 print('p1 is ', p1)
 print('p2 is ', p2)
+
+
+# Pretty good. Only real issue was accidentally
+# Double counting initial cards by doing
+# p2 = len(lines) to initialize. Kind of outsmarted
+# myself with that one. Part 1 was my best placement
+# Thus far though, so that's nice.
